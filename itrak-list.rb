278b4f78a319044b93052e3eb1fed16cd568727e
Jack Flannery <jmflannery81@gmail.com>
2016-09-05 00:14:14 -0400

    Add id to list

--- itrak-list.rb
+++ itrak-list.rb
@@ -8,6 +8,7 @@ $us = -> (flt) {
 }
 
 incomes = wallet.data.map { |i| {
+  id: i.id,
   name: i.name,
   amount: i.amount.to_f,
   date: i.date.iso8601,
@@ -31,6 +32,7 @@ incomes.reduce(totals) do |t, i|
   t
 end
 
+id_width = incomes.map { |i| i[:id] }.max.to_s.size || 1
 name_width = incomes.map { |i| i[:name].size }.max || 5
 amount_width = $us.call(totals[:amount]).size || 5
 date_width = incomes.map { |i| i[:date].size }.max || 5
@@ -40,12 +42,12 @@ leftover_width = $us.call(totals[:leftover]).size || 5
 
 # Header
 puts
-puts "Name".center(name_width) + " " + "Amount".center(amount_width) + " " + "Date".center(date_width) + " " + "Tax".center(tax_width) + " " + "Save".center(save_width) + " " + "Left".center(leftover_width)
-puts "-" * ((name_width + amount_width + date_width + tax_width + save_width + leftover_width) + 5)
+puts "ID".center(id_width) + " " + "Name".center(name_width) + " " + "Amount".center(amount_width) + " " + "Date".center(date_width) + " " + "Tax".center(tax_width) + " " + "Save".center(save_width) + " " + "Left".center(leftover_width)
+puts "-" * ((id_width + name_width + amount_width + date_width + tax_width + save_width + leftover_width) + 5)
 
 # Income list
 incomes.each do |i|
-  puts i[:name].ljust(name_width) + ' ' + $us.call(i[:amount]).rjust(amount_width) + ' ' + i[:date].ljust(date_width) + ' ' + $us.call(i[:tax]).rjust(tax_width) + ' ' + $us.call(i[:save]).rjust(save_width) + ' ' + $us.call(i[:leftover]).rjust(leftover_width)
+  puts i[:id].to_s.ljust(id_width) + ' ' + i[:name].ljust(name_width) + ' ' + $us.call(i[:amount]).rjust(amount_width) + ' ' + i[:date].ljust(date_width) + ' ' + $us.call(i[:tax]).rjust(tax_width) + ' ' + $us.call(i[:save]).rjust(save_width) + ' ' + $us.call(i[:leftover]).rjust(leftover_width)
 end
 
 # Totals
